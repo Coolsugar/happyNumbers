dd3347c0f4c6fe6ce00fd42ff84ac005e74500b2
Esraaj Sarkar Gupta <tsampi.24@gmail.com>
2024-06-27 02:07:05 +0530

    Update Happy_Numbers.py - Needed comments and minor changes

--- Happy_Numbers.py
+++ Happy_Numbers.py
@@ -1,14 +1,17 @@
 print('Happy Number Generator')
-r = 1 + int(input("Enter range: 0 - "))
+r = 1 + int(input("Enter range: 0 - ")) # Obtain an upper limit
 s = 0 
 i = 0 
-happyNumbers = []
-if r > 1000:
+happyNumbers = [] # Define list to hold values
+
+if r > 1000: # Determine logging preferences 
 	d = 0.000000000001
 else:
 	d = 0.1
+	
 import time as t
-def log(num):
+
+def log(num): # Define logging function 
 	str_happyNumbers = [str(_) for _ in happyNumbers]
 	number_of_elements = len(happyNumbers)
 	fl = open("/Users/esraaj/Documents/Code/Maths/happyNumLog.txt" , 'a') 
@@ -24,26 +27,31 @@ def log(num):
 	fl.write("\n =============")
 	print("List file updated.")
 	print("Ratio: " , number_of_elements , "/" , num )
-def split(word):
-	List = list(word)
+	
+def split(number): # Define split and sum function
+	List = list(number)
 	fixList = [int(x) for x in List]
 	return fixList
-def succ(num):
+	
+def succ(num): # Define success 
 	t.sleep(d)
 	print('-> ',num)
 	happyNumbers.append(num)
 	if num + 1 == r:
 		print(happyNumbers)
 		log(num)
-def fail(num):
+		
+def fail(num): # Define failure
 	if num + 1 == r:
 		print(happyNumbers)
 		log(num)
-for num in range(r):		
-	l = split(str(num))			
-	while 0 == 0:
-		for elm in l:
+		
+for num in range(r): # Begin interating through every single integrer
+	l = split(str(num)) # Split number into its digits 	
+	while 0 == 0: # Run until a function stops us 
+		for elm in l: 
 			s += (elm**2)
+		# Determine failure or success paramters (got this data from Wolfram Alpha or something)
 		if s == 1:
 			succ(num)
 			l = split(str(s))
